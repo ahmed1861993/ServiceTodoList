ed539c47d0a079184f7e144399919ad57c5c65da
Your Name <you@example.com>
2019-08-07 23:05:15 +0200

    create TodoApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import TodoItems from './task/Todoitems/Todoitems'
+import AddItems from './task/Additems/Additems'
+import Home from './task/Home/Home'
+import {BrowserRouter , Route} from 'react-router-dom';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  state = {
+    items : [
+      
+    ]
+  }
+  deleteItem =  (id) =>{
+let items=this.state.items;
+let i =items.findIndex(item => item.id === id)
+items.splice(i,1)
+this.setState({items})
+  }
+  AddItems=(item) =>{
+    item.id = Math.random();
+    let items=this.state.items;
+    items.push(item);
+    this.setState({items})
+  };
+  render(){
+    return (
+
+      
+      <div className="App ">
+
+      <Home/>
+     
+      <div className="container">
+     <h1 className="text-center">We Help Throuth</h1>
+     <AddItems AddItems={this.AddItems}/>
+       <TodoItems items={this.state.items} deleteItem={this.deleteItem}/>
+       </div>
+
+
+       
+      </div>
+    );
+  }
+} 
+
+ 
+
 
 export default App;
